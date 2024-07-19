17a6450ea4c79c529017157a88eec0544d7b57b6
alek <aleksandrgaranin89@gmail.com>
2024-07-19 13:00:59 -0700

    changed magic button to the different style

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -73,18 +73,8 @@ const Hero = () => {
             {selectedLanguage.nameIntioduction}
           </p>
           <div className='flex mt-5 sm:m-5 flex-row flex-wrap gap-5 justify-center'>
-
-            <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] 
-            focus:outline-none md:w-60 md:mt-10" onClick={toggleLanguage}>
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] 
-              bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className={`inline-flex h-full w-full cursor-pointer items-center 
-              justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium
-            text-white backdrop-blur-3xl gap-2 `}>
-                {language === 'en' ? "Change language to Russian" : 'Поменять язык на Английский'}<FaRightLeft />
-              </span>
-              
-            </button>
+            <MagicButton
+              title={language === 'en' ? "Change language to Russian" : 'Поменять на Английский'} icon={<FaRightLeft />} position={'right'} handleClick={toggleLanguage} />
             <a href='#about'>
               <MagicButton
                 title={selectedLanguage.buttonText} icon={<FaLocationArrow />} position={'right'} />
